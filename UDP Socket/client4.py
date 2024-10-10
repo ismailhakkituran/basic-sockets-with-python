a189261e6ba61c47209914972d4775c6ef5b8b24
ismailhakkituran <hakki.turan@bil.omu.edu.tr>
2024-10-10 15:38:20 +0300

    2425-fall

--- UDP Socket/client4.py
+++ UDP Socket/client4.py
@@ -1,16 +1,15 @@
-from socket import * 
+import time
+from socket import *
 
 serverName = 'localhost'
-serverPort = 12000 
-clientSocket = socket (AF_INET, SOCK_DGRAM) 
+serverPort = 12000
+clientSocket = socket (AF_INET, SOCK_DGRAM)
 
 
 
-while True: 
+while True:
     message = 'dddddddddd'
-    clientSocket.sendto(message.encode(), (serverName, serverPort)) 
+    clientSocket.sendto(message.encode(), (serverName, serverPort))
     modifiedMessage, serverAddress = clientSocket.recvfrom (2048)
-    print (modifiedMessage.decode()) 
-
-
-clientSocket.close()
+    print (modifiedMessage.decode())
+    time.sleep(1)
